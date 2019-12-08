44ed8b4aedfa1e0ecae8ff0060a7eb402aa65316
Oscar <oscar.leon@4agile.cl>
2019-12-08 01:59:51 -0300

    feat: consume api coincap and views

---
+++ src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <px-assets-table :assets="assets" />
+  </div>
+</template>
+
+<script>
+import api from '@/utils/api'
+import PxAssetsTable from '@/components/PxAssetsTable'
+
+export default {
+  name: 'Home',
+  components: { PxAssetsTable },
+  data() {
+    return {
+      assets: []
+    }
+  },
+
+  created() {
+    api.getAssets().then(response => (this.assets = response))
+  }
+}
+</script>
