dbba6ce2fe7e5f539a367f66d1ffd9d7c1bf2a6b
Oscar <oscar.leon@4agile.cl>
2019-12-10 00:11:27 -0300

    feat: logic button change for view detail coin

--- src/views/CoinDetail.vue
+++ src/views/CoinDetail.vue
@@ -50,12 +50,19 @@
 
         <div class="my-10 sm:mt-0 flex flex-col justify-center text-center">
           <button
+            @click="toogleConverted"
             class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
-          >Cambiar</button>
+          >
+            {{
+            fromUsd ? `USD to ${asset.symbol}` : `${asset.symbol} to USD`
+            }}
+          </button>
 
           <div class="flex flex-row my-5">
             <label class="w-full" for="convertValue">
               <input
+                v-model="convertValue"
+                :placeholder="`Valor en ${fromUsd ? 'USD': asset.symbol}`"
                 id="convertValue"
                 type="number"
                 class="text-center bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block w-full appearance-none leading-normal"
@@ -63,7 +70,7 @@
             </label>
           </div>
 
-          <span class="text-xl"></span>
+          <span class="text-xl">{{convertResult}} {{fromUsd? asset.symbol: 'USD'}}</span>
         </div>
       </div>
 
@@ -120,11 +127,23 @@ export default {
       isLoading: false,
       asset: {},
       history: [],
-      markets: []
+      markets: [],
+      fromUsd: true,
+      convertValue: null
     }
   },
 
   computed: {
+    convertResult() {
+      if (!this.convertValue) {
+        return 0
+      }
+      const result = this.fromUsd
+        ? this.convertValue / this.asset.priceUsd
+        : this.convertValue * this.asset.priceUsd
+      return result.toFixed(2)
+    },
+
     min() {
       return Math.min(
         ...this.history.map(h => parseFloat(h.priceUsd).toFixed(2))
@@ -155,6 +174,9 @@ export default {
   },
 
   methods: {
+    toogleConverted() {
+      this.fromUsd = !this.fromUsd
+    },
     getWebSite(exchange) {
       this.$set(exchange, 'isLoading', true)
 
